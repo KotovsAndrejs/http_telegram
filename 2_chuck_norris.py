006e17995252bbfed4bfb4ba326cf49e598ae37c
KotovsAndrejs <125635760+KotovsAndrejs@users.noreply.github.com>
2024-04-17 13:23:28 +0300

    Update 2_chuck_norris.py

--- 2_chuck_norris.py
+++ 2_chuck_norris.py
@@ -8,3 +8,23 @@
 # 2. Izdrukā 3 nejaušus jokus no Chuck Norris par programmēšanu
 # 
 import requests
+
+
+while True:
+    print("\nNejaušo datu ģenerators:")
+    print("1. Izdrukā 3 nejaušus jokus no Chuck Norris")
+    print("2. Izdrukā 3 nejaušus jokus no Chuck Norris par programmēšanu")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        for i in range(3):
+            r = requests.get('https://api.chucknorris.io/jokes/random')
+            joke = r.json()
+            print(joke["value"])
+        pass
+    elif choice == "2":
+        for i in range(3):
+            r = requests.get('https://api.chucknorris.io/jokes/random?category=dev')
+            joke = r.json()
+            print(joke["value"])
+        pass
