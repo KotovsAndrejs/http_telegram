5b88ea1a0f19228954a407d637809c127018e4b7
KotovsAndrejs <125635760+KotovsAndrejs@users.noreply.github.com>
2024-04-17 13:23:54 +0300

    Update 3_telegram-bot.py

--- 3_telegram-bot.py
+++ 3_telegram-bot.py
@@ -18,9 +18,11 @@
 
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
+from faker import Faker
+import requests
+fake = Faker()
 # izveido bota pieslēgumu Telegram
-app = ApplicationBuilder().token("YOUR_TOKEN").build()
+app = ApplicationBuilder().token("6501099925:AAHr2X9g1QdM82VWIcNFBK2_p_0Gym6I6RI").build()
 
 # komanda /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -29,16 +31,28 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # komanda /hello
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(update.effective_user)
-    await update.message.reply_text("Hello", update.effective_user.first_name, " ", update.effective_user.last_name)
+    await update.message.reply_text("Hello " + update.effective_user.first_name)
 
 # komanda /echo
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("I hear: " + update.message.text)
 
+# komanda /fakeperson
+async def fakeperson(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Fake person: " + fake.name() + " " + fake.phone_number() + " " + fake.street_address() + " " + fake.ssn())
+
+# komanda /chuck
+async def chuck(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    r = requests.get('https://api.chucknorris.io/jokes/random')
+    joke = r.json()
+    await update.message.reply_text("Joke: " + joke["value"])
+
 # savieno čata komandu ar funkciju
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("echo", echo))
+app.add_handler(CommandHandler("fakeperson", fakeperson))
+app.add_handler(CommandHandler("chuck", chuck))
 
 # sāk bota darbību
 app.run_polling()
